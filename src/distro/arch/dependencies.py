6b24778df11398c3f5510ea86fcad4f3db95f3f5
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-02-13 11:47:49 +0530

    install dependencies

---
+++ src/distro/arch/dependencies.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+import src.utils.db as db
+import src.utils.dirs as dirs
+
+
+def install_main():
+    os.system("sudo pacman -Sy git base-devel")
+
+
+def install_yay():
+    dirs.chdirMain()
+    os.system("git clone https://aur.archlinux.org/yay.git")
+    os.chdir(os.path.join(os.getcwd(), "yay"))
+    os.system("makepkg -si")
