2f64b88b367fab7beadbafc67650b411aa053688
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-02-13 19:50:17 +0530

    run snap commands

--- src/distro/debian/custom.py
+++ src/distro/debian/custom.py
@@ -14,3 +14,15 @@ def runCustom(args: list = None):
     ))
 
     os.system(f"sudo apt {final_command}")
+
+
+def runSnap(args: list = None):
+    if args is None:
+        sys.exit("[E] Please enter the args")
+
+    final_command = utils.process_final_command(args=args, checks=(
+        "wtf",
+        "./wtf"
+    ))
+
+    os.system(f"sudo snap {final_command}")
