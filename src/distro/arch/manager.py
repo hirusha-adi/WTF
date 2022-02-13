f1a22cbbe50804ae2ba5087d11e3e9208b8effc9
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-02-13 14:46:33 +0530

    new subcommands

--- src/distro/arch/manager.py
+++ src/distro/arch/manager.py
@@ -6,7 +6,22 @@ from . import dependencies, install, remove, showlist, updates, custom
 
 def runArchInstaller():
     all_args = sys.argv[1:]
+
+    # Install program
     if all_args[0].strip() == "install":
         install.installPackage(args=all_args[1:])
-    if (all_args[0].strip() == "installf") or (all_args[0].strip() == "finstall"):
+
+    # Force Install (install without confirmation)
+    elif (all_args[0].strip() in ("installf", "finstall", "force-install",
+                                  "forceinstall", "installforce",
+                                  "install-force")):
         install.installPackageForce(args=all_args[1:])
+
+    # Remove program
+    elif (all_args[0].strip() in ("remove", "uninstall")):
+        remove.removePackage(args=all_args[1:])
+
+    # Remove Program with dependencies
+    elif (all_args[0].strip() in ("allremove", "removeall", "all-remove",
+                                  "remove-all", "removea")):
+        remove.removePackageWithDependencies(args=all_args[1:])
