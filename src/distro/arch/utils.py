7f63f580e5c8b5cc2460c4279abe03f69529e082
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-02-13 16:14:03 +0530

    optimised + removed unwanted code

--- src/distro/arch/utils.py
+++ src/distro/arch/utils.py
@@ -1,13 +1,17 @@
 import os
 
 
-def process_final_command(args: list = None, checks: tuple = None):
-    final_command = ""
-    for arg in args:
-        for check in checks:
-            if str(arg).strip().startswith(check):
-                continue
-            if str(arg).strip().startswith(check):
-                continue
-        final_command += f"{arg} "
-    return final_command
+def process_final_command(args=None, checks=None):
+    if not(checks is None):
+        final_command = ""
+        for arg in args:
+            for check in checks:
+                if str(arg).strip().startswith(check):
+                    continue
+            final_command += f"{arg} "
+        return final_command
+    else:
+        final_command = ""
+        for arg in args:
+            final_command += f"{arg} "
+        return final_command
