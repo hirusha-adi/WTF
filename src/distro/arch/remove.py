d8a90bb3defd13822508ddf0f8abcb912a8e9268
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-02-13 14:45:21 +0530

    the functions can now be used alone

--- src/distro/arch/remove.py
+++ src/distro/arch/remove.py
@@ -19,10 +19,15 @@ def removePackage(args: list = None):
     os.system(f"yay -R {final_command}")
 
 
-def removePackageWithDependencies():
-    try:
-        program_names_all = sys.argv[2:]
-    except IndexError:
+def removePackageWithDependencies(args: list = None):
+    if args is None:
         sys.exit("[E] Please enter the package names to remove")
-    for program_name in program_names_all:
-        os.system(f"yay -Rs {program_name}")
+
+    final_command = ""
+    for arg in args:
+        # to prevent passing the same argument twice
+        if str(arg).strip().startswith("-R"):
+            continue
+        final_command += f"{arg} "
+
+    os.system(f"yay -Rs {final_command}")
