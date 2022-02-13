df76afc80d1d90dade0748d08c63afa04d37d7e2
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-02-13 15:55:59 +0530

    improved efficiency

--- src/distro/arch/install.py
+++ src/distro/arch/install.py
@@ -3,19 +3,17 @@ import sys
 
 # import src.utils.db as db
 # import src.utils.dirs as dirs
-from . import custom
+from . import custom, utils
 
 
 def installPackage(args: list = None):
     if args is None:
         sys.exit("[E] Please enter the package names to install")
 
-    final_command = ""
-    for arg in args:
-        # to prevent passing the same argument twice
-        if str(arg).strip().startswith("-S"):
-            continue
-        final_command += f"{arg} "
+    final_command = utils.process_final_command(args=args, checks=(
+        "-S",
+        "-Sy"
+    ))
 
     if "betterdiscord" in final_command:
         custom.install_betterDiscord()
@@ -28,14 +26,10 @@ def installPackageForce(args: list = None):
     if args is None:
         sys.exit("[E] Please enter the package names to install")
 
-    final_command = ""
-    for arg in args:
-        # to prevent passing the same arguments twice
-        if str(arg).strip().startswith("-S"):
-            continue
-        if str(arg).strip().startswith("--noconfirm"):
-            continue
-        final_command += f"{arg} "
+    final_command = utils.process_final_command(args=args, checks=(
+        "-S",
+        "--noconfirm"
+    ))
 
     if "betterdiscord" in final_command:
         custom.install_betterDiscord()
