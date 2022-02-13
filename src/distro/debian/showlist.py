9a75f15cc697bd3c98087422b0a6c09a43e084b9
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-02-13 19:30:41 +0530

    removed unwanted code

--- src/distro/debian/showlist.py
+++ src/distro/debian/showlist.py
@@ -1,11 +1,5 @@
 import os
 
 
-def listPackages(lookingfor=None):
-    if lookingfor is None:
-        os.system("dpkg --list")
-    else:
-        if isinstance(lookingfor, str):
-            os.system(f"dpkg --list | grep {lookingfor}")
-        else:
-            os.system(f"dpkg --list | grep {' '.join(lookingfor)}")
+def listPackages():
+    os.system("sudo dpkg --list")
