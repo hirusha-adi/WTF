5163d78a6655b78b714b31043306f3464b1fc9e4
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-02-13 14:40:38 +0530

    not working solely

--- src/distro/arch/remove.py
+++ src/distro/arch/remove.py
@@ -5,13 +5,18 @@ import sys
 # import src.utils.dirs as dirs
 
 
-def removePackage():
-    try:
-        program_names_all = sys.argv[2:]
-    except IndexError:
+def removePackage(args: list = None):
+    if args is None:
         sys.exit("[E] Please enter the package names to remove")
-    for program_name in program_names_all:
-        os.system(f"yay -R {program_name}")
+
+    final_command = ""
+    for arg in args:
+        # to prevent passing the same argument twice
+        if str(arg).strip().startswith("-R"):
+            continue
+        final_command += f"{arg} "
+
+    os.system(f"yay -R {final_command}")
 
 
 def removePackageWithDependencies():
