def18d2a4999e1495b64610cb896804c9bfb85b1
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-02-23 23:16:29 +0530

    add help

--- src/program.py
+++ src/program.py
@@ -1,13 +1,18 @@
 import src.distro.arch.manager as arch_manager
 import src.distro.debian.manager as debian_manager
-from utils import distro
+from src.utils import distro
+from src.utils import helps
 
 
 def startProgram():
     distro_cur = distro.checkDistro_Simple()
-    if distro_cur == "ubuntu":
-        debian_manager.runDebianInstaller()
-    elif distro_cur == "debian":
-        debian_manager.runDebianInstaller()
+    if (distro_cur == "ubuntu") or (distro_cur == "debian"):
+        try:
+            debian_manager.runDebianInstaller()
+        except IndexError:
+            print(helps.__DEBIAN__)
     elif distro_cur == "arch":
-        arch_manager.runArchInstaller()
+        try:
+            arch_manager.runArchInstaller()
+        except IndexError:
+            print(helps.__ARCH__)
