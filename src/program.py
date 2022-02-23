79bda9be596c32b25b6160b8e8339156a2ed1352
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-02-23 22:55:41 +0530

    start program

--- src/program.py
+++ src/program.py
@@ -1,1 +1,13 @@
-from src.utils import distro
+import src.distro.arch.manager as arch_manager
+import src.distro.debian.manager as debian_manager
+from utils import distro
+
+
+def startProgram():
+    distro_cur = distro.checkDistro_Simple()
+    if distro_cur == "ubuntu":
+        debian_manager.runDebianInstaller()
+    elif distro_cur == "debian":
+        debian_manager.runDebianInstaller()
+    elif distro_cur == "arch":
+        arch_manager.runArchInstaller()
