75245c3c80e874d430b6880ba5cba973d7477d12
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-02-23 23:16:12 +0530

    custom help messages

---
+++ src/utils/helps.py
@@ -0,0 +1,39 @@
+__DEBIAN__ = r"""
+__        _______ _____  
+\ \      / /_   _|  ___| 
+ \ \ /\ / /  | | | |_    Simple Package
+  \ V  V /   | | |  _|   Manager Wrapper
+   \_/\_/    |_| |_|    
+
+Commands:
+    install         Install a Package
+    installf        Install a Package without Confirmation
+    remove          Remove a Package
+    removeall       Remo a Package with Saved Data
+    list            Display all installed packages
+    update          Update Repo's
+    upgrade         Upgrade System Packages
+    upgradeforce    Upgrade System Packages without Confirmation
+    snap            Run a custom snap command
+    setup           Setup WTF
+"""
+
+__ARCH__ = r"""
+__        _______ _____  
+\ \      / /_   _|  ___| 
+ \ \ /\ / /  | | | |_    Simple Package
+  \ V  V /   | | |  _|   Manager Wrapper
+   \_/\_/    |_| |_|    
+
+Commands:
+    install         Install a Package
+    installf        Install a Package without Confirmation
+    remove          Remove a Package
+    removeall       Remo a Package with Saved Data
+    list            Display all installed packages
+    update          Update Repo's
+    updatef         Update Repo's (Force)
+    upgrade         Upgrade all packages
+    upgradef        Update and Upgrade System
+    setup           Setup WTF
+"""
